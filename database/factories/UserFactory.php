a708312959ae3aad528dc915f1d1eb57ee0aba64
zulkifli_r <muh.zulkifli@docotel.com>
2019-06-24 16:22:04 +0800

    seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Support\Arr;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,15 +18,20 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$jenis_akun = DB::table('jenis_akun')->pluck('id')->toArray();
+
+$factory->define(User::class, function (Faker $faker) use ( $jenis_akun ) {
     return [
         'uuid' => Str::uuid(),
-        'name' => $faker->name,
+        'user_id' => $faker->creditCardNumber,
+        'nama_lengkap' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'tipe_user' => Arr::random(['pelatihan','pengujian','pegawai']),
-        'tipe_user_forum' => Arr::random([1,2,3,4,5,6]),
+        'pekerjaan' => $faker->jobTitle,
+        'instansi' => $faker->company,
+        'no_telepon' => $faker->tollFreePhoneNumber,
+        'jenis_akun' => Arr::random($jenis_akun),
         'remember_token' => Str::random(10),
     ];
 });
