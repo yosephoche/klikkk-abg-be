73b4ae3036d380c7e8aed5cd1fb61f8a6f738790
muhaditya <muh.aditya@docotel.com>
2019-08-28 15:18:33 +0800

    pertanyaan lagi

--- app/Http/Controllers/Api/User/Pengajuan/qnaController.php
+++ app/Http/Controllers/Api/User/Pengajuan/qnaController.php
@@ -82,14 +82,15 @@ class qnaController extends Controller
         $answer->qna_id = $id;
         
         $question = QnA::find($id);
-        // $latest = $question->answers->last();
+        $latest = $question->answers->last();
 
-        // if(!isset($latest))
-        // {
-        //     $user = User::find($question->user->id);
-        // } else {
-        //     $user = User::find($latest->user->id);
-        // }
+        if(!isset($latest))
+        {
+            $user = User::find($question->user->id);
+        } else {
+            $user = User::find($latest->user->id);
+        }
+
 
         // $role = $user->roles->where('name','staf_teknis')->first();
         // if(isset($role))
@@ -104,7 +105,11 @@ class qnaController extends Controller
         // }       
         
         $question->answers()->save($answer);
-        Notification::send($question->user, new answerPostNotification($answer,$question));        
+
+        if($answer->user->id !== $user->id )
+        {
+            Notification::send($question->user, new answerPostNotification($answer,$question));        
+        }
         return dtcApiResponse(200,null,'Pertanyaan Sudah Di Balas');
     }
 
