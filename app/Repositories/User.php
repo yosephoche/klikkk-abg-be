bc8f2a03daa9895279b2cdb7fcac6c612903fc5b
muhaditya <muh.aditya@docotel.com>
2019-09-06 06:31:52 +0800

    test

--- app/Repositories/User.php
+++ app/Repositories/User.php
@@ -318,6 +318,7 @@ class User extends BaseRepository
         }
         $user->nama_lengkap = $data->name;
         $user->save();
+        dd($user);
         return $user->avatar?asset('storage'.$user->avatar):$user;
     }
 
