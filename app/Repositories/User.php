1c6cb1ff91ffb646873f162fc80d9c215fa90259
muhaditya <muh.aditya@docotel.com>
2019-09-06 10:16:29 +0800

    link avatar

--- app/Repositories/User.php
+++ app/Repositories/User.php
@@ -318,6 +318,7 @@ class User extends BaseRepository
         }
         $user->nama_lengkap = $data->name;
         $user->save();
+        $user['avatar'] = asset('storage'.$user->avatar);
         return $user;
     }
 
